e97a28fb1da48c7768ace4e5ab652cdcf950d70d
milana-kasim <milana.kasim@city.ac.uk>
2024-10-22 19:51:03 +0100

    Files added

---
+++ HW 4/task2.sql
@@ -0,0 +1,21 @@
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
+VALUES ('Fluffy', '2020-10-15', 'vet', 'antibiotics');
+
+INSERT INTO petPet (petname, owner, species, gender, birth, death)
+VALUES ('Hammy', 'Diane', 'hamster', 'M', '2010-10-30', NULL);
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
+VALUES ('Hammy', '2020-10-15', 'vet', 'antibiotics');
+
+UPDATE petEvent
+Set remark = '5 kittens, 3 female, 2 male'
+WHERE petname = 'Fluffy' AND eventdate = '1995-05-15' AND eventtype = 'litter';
+
+UPDATE petEvent
+SET petname = 'Claws'
+WHERE petname = 'Slim' AND eventdate = '1997-08-03' AND eventtype = 'vet';
+
+UPDATE petPet
+SET death = '2020-09-01'
+WHERE petname = 'Puffball';
+
+DELETE FROM petPet WHERE petname = 'Buffy';
